bd83be994907966c04578fc7be7d619591813d70
Sarasecellterra <saskeloph@gmail.com>
2024-10-29 14:39:48 +0000

    Extra button for adding to cart.

--- Ecommerce1/Controllers/CartController.cs
+++ Ecommerce1/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
 
 public class CartController : Controller
 {
@@ -19,11 +20,11 @@ public class CartController : Controller
         {
             connection.Open();
 
-            // Join Cart_item and Product tables to get product names and other details
+            //Command to read from the database.
             SqlCommand command = new SqlCommand(
-                @"SELECT c.id_cart, c.Quantity, p.Name, p.Price 
-                  FROM Cart_Item c 
-                  JOIN Product p ON c.id_product = p.id_product", connection);
+                @"SELECT c.Id_cart, c.Quantity, p.Name, p.Price 
+                  FROM CartItems c 
+                  JOIN Product p ON c.Id_product = p.Id_product", connection);
 
             SqlDataReader reader = command.ExecuteReader();
             while (reader.Read())
